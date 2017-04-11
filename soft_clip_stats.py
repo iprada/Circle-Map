ceb40241d18fe73debd3b5816f10a977cad3143f
iprada <iprada@sund.ku.dk>
2017-04-11 21:42:48 +0200

    create a modify file for computing the stats of the soft clip reads

--- soft_clip_stats.py
+++ soft_clip_stats.py
@@ -1,12 +1,12 @@
 import pysam as ps
 import os
 import matplotlib.pyplot as plt
-os.chdir("/home/inigo/msc_thesis")
+os.chdir("/home/inigo/msc_thesis/mapping_stats/")
 
 
 
 #sort the bam file by query name, the reads will be sorted in the order they were generated
-#os.system("samtools sort -n -o query_name_sorted_paired_end_sim_aln.bam.sv.bam sorted_paired_end_sim_aln.bam.sv.bam")
+os.system("samtools sort -n -o query_name_sorted_paired_end_sim_aln.bam.sv.bam sorted_paired_end_sim_aln.bam.sv.bam")
 
 
 #open the bam file
@@ -41,28 +41,37 @@ soft_clipped = ps.Samfile("soft_clipped.bam", "rb")
 #file of the unique soft-clipped, meaning that they only have mapped to one part of the breakpoint
 uniq_soft_cliped_bam = ps.AlignmentFile("unique_soft_clipped.bam","wb",template=samfile)
 
-first_read = uniq_soft_cliped_bam.head(1)
+
+#build an index so that one can find the how many times in each read present
+
+index = ps.IndexedReads(soft_clipped)
+index.build()
 
 
 
 
 
-previous_read = None
+#get query names of the reads that are unique
+os.system("samtools view soft_clipped.bam | awk '{print $1}' | uniq -c | awk '{if ($1==1) {print $2}}' > uniq_soft_clip.txt")
 
-for read in first_read:
-    previous_read = read.query_name
+soft_clip_queries = open("uniq_soft_clip.txt","r")
+soft_clip_queries = soft_clip_queries.readlines()
 
 
-for read in soft_clipped:
-    if previous_read == read.query_name:
-        continue
-    else:
+
+for query in soft_clip_queries:
+    iterator = index.find(query.strip())
+    for read in iterator:
         uniq_soft_cliped_bam.write(read)
-
-    previous_read = read.query_name
 
 
 uniq_soft_cliped_bam.close()
+
+#remove intermediate files
+os.system("rm uniq_soft_clip.txt")
+os.system("rm query_name_sorted_paired_end_sim_aln.bam.sv.bam")
+os.system("rm soft_clipped.bam")
+
 
 
 uniq_soft_cliped_bam = ps.AlignmentFile("unique_soft_clipped.bam","rb")
@@ -83,11 +92,13 @@ for read in uniq_soft_cliped_bam:
                 continue
 
 
-print(len(soft_clip_len))
+uniq_soft_cliped_bam.close()
 
-plt.hist(soft_clip_len,bins=70)
+
+plt.hist(soft_clip_len,bins=30)
 plt.ylabel("Frequency")
 plt.xlabel("Soft clip length")
-plt.show()
-
+plt.title("%s single soft-clip" % len(soft_clip_queries))
+plt.savefig('soft_clip_len.png')
+plt.close()
 
