89c6c04bc442fcc5d81137339eeb2d9506f91fee
iprada <iprada@sund.ku.dk>
2017-04-13 18:53:13 +0200

    create script for checking that reads are mapped correctly

---
+++ check_mapping.py
@@ -0,0 +1,13 @@
+import pysam as ps
+import pandas as pd
+import os
+
+
+
+os.chdir("/home/inigo/msc_thesis/mapping_stats/")
+#os.system("samtools sort -n -o query_name_sorted_paired_end_sim_aln.bam.sv.bam sorted_paired_end_sim_aln.bam.sv.bam")
+samfile = ps.Samfile("query_name_sorted_paired_end_sim_aln.bam.sv.bam", "rb")
+
+for read in samfile:
+    print(read.query_name)
+    break
