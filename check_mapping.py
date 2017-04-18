603622ad77ad990d5e16ed595ac121c4bf62b3bb
iprada <iprada@sund.ku.dk>
2017-04-18 11:09:28 +0200

    a

--- check_mapping.py
+++ check_mapping.py
@@ -8,8 +8,3 @@ os.chdir("/home/inigo/msc_thesis/mapping_stats/")
 #os.system("samtools sort -n -o query_name_sorted_paired_end_sim_aln.bam.sv.bam sorted_paired_end_sim_aln.bam.sv.bam")
 samfile = ps.Samfile("query_name_sorted_paired_end_sim_aln.bam.sv.bam", "rb")
 
-for read in samfile:
-    query = read.query_name
-    query_info = query.split('|')
-
-    break
