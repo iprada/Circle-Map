8e26402489d55052227a8cb5708cf337060c7f61
iprada <iprada@sund.ku.dk>
2017-05-08 17:06:59 +0200

    aaa

--- alignment.py
+++ alignment.py
@@ -6,12 +6,8 @@ import pybedtools as bt
 import multiprocessing as mp
 
 
-
-
-
 class alignment:
-
-    def __init__(self,bam,working_dir):
+    def __init__(self, bam, working_dir):
         self.bam = bam
         self.circ_bam = "circ_calls.bam"
         self.circ_bedgraph = "circ_dna_boundaries.bedGraph"
@@ -20,49 +16,42 @@ class alignment:
         self.number_of_cores = 3
         self.circ_boundaries = bt.BedTool("read_coverage_merged.bed")
 
-
     def remove_concordant_pairs(self):
         """Function that removes concordant pairs"""
-        os.system("samtools view -hb -F 2 %s > %s" % (self.bam,self.circ_bam))
-
+        os.system("samtools view -hb -F 2 %s > %s" % (self.bam, self.circ_bam))
 
     def query_name_sorted_circs(self):
         os.system("samtools sort -n -o query_name_sorted_circs.bam circ_calls.bam")
         os.system("samtools index circ_calls.bam ")
-        sorted_circs = ps.AlignmentFile("query_name_sorted_circs.bam","rb")
-        return(sorted_circs)
+        sorted_circs = ps.AlignmentFile("query_name_sorted_circs.bam", "rb")
+        return (sorted_circs)
 
-    def check_mapping_range(self,interval,read):
-        if read.reference_start  >= (interval.start - 50):
-                if read.reference_start <= (interval.end +50):
+    def check_mapping_range(self, interval, read):
+        if read.reference_start >= (interval.start - 50):
+            if read.reference_start <= (interval.end + 50):
+                return (True)
 
-                    return(True)
-                
         else:
 
-            return(False)
+            return (False)
 
-
-    def is_soft_clipped(self,read):
+    def is_soft_clipped(self, read):
         for cigar in read.cigar:
             if cigar[0] == 4:
-                return(True)
+                return (True)
             else:
-                return(False)
+                return (False)
 
     def generate_bed_from_circ_calls(self):
-        #bedtools genomecov -bg -ibam circ_calls.bam | mergeBed > circ_support_calls.bed
-        return(None)
-
-
-
+        # bedtools genomecov -bg -ibam circ_calls.bam | mergeBed > circ_support_calls.bed
+        return (None)
 
     def call_circles(self):
         """Function that aims to call circles based on the boundaries identified using the discordant reads and soft clipped reads"""
         print("Hunting circles")
-        #parameters to run in parallel
+        # parameters to run in parallel
         number_of_rows = len(self.circ_boundaries)
-        l = range(1,number_of_rows)
+        l = range(1, number_of_rows)
         medium_process = number_of_rows / self.number_of_cores
         medium_process = (int(medium_process))
         n = medium_process
@@ -74,23 +63,16 @@ class alignment:
 
         spawn_reads = [l[i:i + n] for i in [0, len(l), n]]
 
-
-
-
         # iterate trough each entry in the bed file
 
-        #file of the sorted circles
+        # file of the sorted circles
         os.system("samtools index circ_calls.bam")
-        circs = ps.AlignmentFile("circ_calls.bam","rb")
+        circs = ps.AlignmentFile("circ_calls.bam", "rb")
 
         # bam file of all reads
         all_bam = ps.AlignmentFile(self.all_bam, "rb")
 
-
-
-
-
-        #mapq_filter
+        # mapq_filter
         mapq_filtered = []
         each_one = 0
         for interval in self.circ_boundaries:
@@ -106,7 +88,7 @@ class alignment:
                     each_one += 1
 
             else:
-                each_one +=1
+                each_one += 1
                 mapq_filtered.append(interval)
                 continue
 
@@ -115,24 +97,21 @@ class alignment:
         mapq_filtered = bt.BedTool(mapq_filtered)
         mapq_filtered.saveas("mapq_filter.bed")
 
-
         filtered_intervals = []
-        #soft_clip_filtering
+        # soft_clip_filtering
         second_filters = 0
         for interval in mapq_filtered:
-            suports = circs.count(interval.chrom,interval.start,interval.end)
+            suports = circs.count(interval.chrom, interval.start, interval.end)
             if (interval.end - interval.start) < 300:
                 if suports > 0:
                     filtered_intervals.append(interval)
-                    second_filters +=1
+                    second_filters += 1
                     print(second_filters)
             else:
                 if suports > 0:
                     filtered_intervals.append(interval)
                     second_filters += 1
                     print(second_filters)
-
-
 
         filtered_intervals = bt.BedTool(filtered_intervals)
 
@@ -142,9 +121,8 @@ class alignment:
         for interval in filtered_intervals:
             boundaries = circ_boundaries.count_hits(interval)
             each_overlapping_interval = circ_boundaries.all_hits(interval)
-            i +=1
+            i += 1
             print(i)
-
 
         # parallel the interval part
 
@@ -152,7 +130,5 @@ class alignment:
 
 
 
-        #save the first file
+        # save the first file
         filtered_intervals.saveas("filtered_cutoff.bed")
-
-
