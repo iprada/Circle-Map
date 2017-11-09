594b31f71f3b0ccfe937320fbeb21337391f8d80
iprada <iprada@sund.ku.dk>
2017-11-09 16:33:21 +0100

    Coded the argparse part of read extractor, include error and warning and regular messages in readExtractor. This should be near the preliminary final version of ReadExtractor. test.py modified for different tests

--- Circle-Map.py
+++ Circle-Map.py
@@ -1,55 +1,189 @@
 #!/data/xsh723/anaconda/bin/python3.6
 
-#Author Inigo Prada Luengo
-#email: inigo.luengo@bio.ku.dk
-
-
-__author__ = [
-    'Inigo Prada-Luengo (inigo.luengo@bio.ku.dk)',
-]
-
-__uni__ = [
-    'University of Copenhagen (Department of Biology)',
-]
 
 import argparse
+import sys
 import os
-
-def main():
-    parser = argparse.ArgumentParser()
-    #check this
+import time
+from extract_circle_SV_reads import readExtractor
 
 
+class circle_map(object):
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(
+            description='Circle-Map',
+            usage='''CircleMap <subprogram> [options]
+
+Author= Inigo Prada-Luengo
+version=1.0
+contact= https://github.com/iprada/Circle-Map/issues
+The Regenberg laboratory
+
+The Circle-Map suite
+
+Commands:
+
+   ReadExtractor     Extracts the reads indicating extrachromosomal circular DNA structural variants
+   Realign         Realign the soft-clipped reads and identify circular DNA
+''')
+        subparsers = self.parser.add_subparsers()
+
+        self.readextractor = subparsers.add_parser(
+            name="ReadExtractor",
+            description='Extracts the reads indicating extrachromosomal circular DNA structural variants',
+            prog="CircleMap ReadExtractor",
+            usage='''CircleMap ReadExtractor [options]
+
+                     Author= Inigo Prada-Luengo
+                     version=1.0
+                     contact= https://github.com/iprada/Circle-Map/issues
+                     The Regenberg laboratory
+                     '''
+
+                                            )
+
+        self.realigner = subparsers.add_parser(
+            name="Realign",
+            description='Realigns the soft-clipped reads and indentifies circular DNA',
+            prog="CircleMap Realign",
+            usage='''CircleMap Realign [options]
+
+                             Author= Inigo Prada-Luengo
+                             version=1.0
+                             contact= https://github.com/iprada/Circle-Map/issues
+                             The Regenberg laboratory
+                             '''
+
+        )
+
+        if len(sys.argv) <= 1:
+            self.parser.print_help()
+            time.sleep(0.01)
+            sys.stderr.write("\nNo argument given to Circle-Map"
+                             "\nExiting\n")
+            sys.exit(1)
+
+        else:
+            if sys.argv[1] == "ReadExtractor":
+
+                self.subprogram = self.args_readextractor()
+                self.args = self.subprogram.parse_args(sys.argv[2:])
+
+                if self.args.directory[-1:] != "/":
+                    self.args.dir = self.args.directory + "/"
+
+                object = readExtractor(self.args.i,self.args.output,self.args.dir,self.args.nodiscordant,
+                                       self.args.nohardclipped,self.args.nosoftclipped,self.args.verbose,self.subprogram)
+                object.extract_sv_circleReads()
+
+            elif sys.argv[1] == "realign":
+                a = 0
 
 
-def readextractor():
-    from extract_circle_SV_reads import readExtractor
-    parser = argparse.ArgumentParser()
-    parser._action_groups.pop()
-    required = parser.add_argument_group('required arguments')
-    optional = parser.add_argument_group('optional arguments')
 
 
 
 
-    required.add_argument('-i', '--input', help="Query name sorted bam file",default="aaaaaaaaa")
-    args = parser.parse_args()
-
-    optional.add_argument('-o', '--output', help="Output bam file with the reads that indicate circular DNA",default="circle_reads%s" % args.input )
-    optional.add_argument('-dir', '--directory', help="Working directory, default is the working directory",default=os.getcwd())
-    args = parser.parse_args()
-    parser.print_help()
-    #object = readExtractor(args.input,args.output,args.directory)
-    #object.extract_sv_circleReads()
 
 
-def map_circles():
-    a = 0
+    def args_readextractor(self):
 
-def annotate():
-    a = 0
+        parser = self.readextractor
 
-# this is done to know if this file is been run directly by python or imported
+
+        parser._action_groups.pop()
+        required = parser.add_argument_group('required arguments')
+        optional = parser.add_argument_group('optional arguments')
+        # prefixing the argument with -- means it's optional
+        # input and output
+
+
+        required.add_argument('-i', metavar='', help="Input: query name sorted bam file")
+
+
+        if "-i" in sys.argv:
+
+            optional.add_argument('-o', '--output', metavar='',
+                              help="Ouput: Reads indicating circular DNA structural variants",
+                              default="circle_%s" % sys.argv[sys.argv.index("-i") + 1])
+
+            optional.add_argument('-dir', '--directory',metavar='',
+                                  help="Working directory, default is the working directory",
+                                  default=os.getcwd())
+
+            # read extraction options
+            # extract discordant reads
+            optional.add_argument('-nd', '--nodiscordant', help="Turn off discordant (R2F1 oriented) read extraction",
+                                 action='store_true')
+
+            # soft-clipped argument
+            optional.add_argument('-nsc', '--nosoftclipped', help="Turn off soft-clipped read extraction",
+                                  action='store_true')
+            # extract hard-clippped reads
+            optional.add_argument('-nhc', '--nohardclipped', help="Turn off hard-clipped read extraction",
+                                  action='store_true')
+
+            #verbose level
+
+
+            optional.add_argument('-v', '--verbose',type=int, metavar='', help='Verbose level, 1=error,2=warning, 3=message',
+                                  choices=[1, 2, 3],default=3)
+
+        else:
+            optional.add_argument('-o', '--output', metavar='',
+                                  help="Ouput: Reads indicating circular DNA structural variants")
+
+            optional.add_argument('-dir', '--directory', metavar='',help="Working directory, default is the working directory",
+                                  default=os.getcwd())
+
+            # read extraction options
+            # extract discordant reads
+            optional.add_argument('-nd', '--nodiscordant', help="Turn off discordant (R2F1 oriented) read extraction",
+                                  action='store_true')
+
+            # soft-clipped argument
+            optional.add_argument('-nsc', '--nosoftclipped', help="Turn off soft-clipped read extraction",
+                                  action='store_true')
+            # extract hard-clippped reads
+            optional.add_argument('-nhc', '--nohardclipped', help="Turn off hard-clipped read extraction",
+                                  action='store_true')
+
+
+            #verbose level
+
+            optional.add_argument('-v', '--verbose',type=int, metavar='', help='Verbose level, 1=error,2=warning, 3=message. Default=3',
+                                  choices=[1, 2, 3],default=3)
+
+
+            parser.print_help()
+
+            time.sleep(0.01)
+            sys.stderr.write("\nNo input input given to readExtractor, be sure that you are providing the flag '-i'"
+                             "\nExiting\n")
+            sys.exit(1)
+
+
+
+        #parse the commands
+
+
+
+        if len(sys.argv[2:]) == 0:
+            parser.print_help()
+            time.sleep(0.01)
+            sys.stderr.write("\nNo arguments given to read extractor. Exiting\n")
+            sys.exit(1)
+
+        return(parser)
+
+    def fetch(self):
+        parser = argparse.ArgumentParser(
+            description='Download objects and refs from another repository')
+        # NOT prefixing the argument with -- means it's not optional
+        parser.add_argument('repository')
+        args = parser.parse_args(sys.argv[2:])
+        print('Running git fetch, repository=%s' % args.repository)
+
+
 if __name__ == '__main__':
-    main()
-    readextractor()
+    circle_map()
--- extract_circle_SV_reads.py
+++ extract_circle_SV_reads.py
@@ -8,31 +8,91 @@ from __future__ import division
 import pysam as ps
 import os
 import time
+import sys
+import warnings
 from utils import is_soft_clipped,is_hard_clipped
 
 class readExtractor:
-    def __init__(self,sorted_bam,output_bam,working_dir):
+    """Class for managing the read extracting part of circle map"""
+    def __init__(self,sorted_bam,output_bam,working_dir,extract_discordant,extract_soft_clipped,extract_hard_clipped,
+                 verbose,parser
+                 ):
+        #input-output
         self.sorted_bam = sorted_bam
         self.output_bam = output_bam
+        #working place
         self.working_dir = working_dir
+        #read options
+        self.no_discordants = extract_discordant
+        self.no_soft_clipped = extract_soft_clipped
+        self.no_hard_clipped = extract_hard_clipped
+        #verbose level
+        self.verbose = int(verbose)
+        #parser options
 
     def extract_sv_circleReads(self):
+
         """Function that extracts Structural Variant reads that indicate circular DNA,
         The programme with extract soft-clipped reads and R2F1 (<- ->) oriented reads"""
+
         os.chdir(self.working_dir)
 
         #open input bam and create output bam
         raw_bam = ps.AlignmentFile(self.working_dir + self.sorted_bam, "rb")
-        circle_sv_reads = ps.AlignmentFile(self.working_dir + self.output_bam, "wb", template=raw_bam)
 
-        print("Extracting circular structural variants")
+        #check that the tag is present
+        if 'HD' in raw_bam.header:
+
+            #check if bam is qname sorted
+            if raw_bam.header['HD']['SO'] != 'queryname':
+
+                #give some errors if not
+                sys.stderr.write(
+                    "The input bam header says that bam is not sorted by queryname. It is sorted by %s\n\n" % (raw_bam.header['HD']['SO']))
+                sys.stderr.write(
+                    "Sort your bam file queryname with the following command:\n\n\tsamtools sort -n -o output.bam input.bam")
+
+                time.sleep(0.01)
+
+                self.parser.print_help()
+                sys.exit(1)
+        else:
+
+            if self.verbose >=2:
+                warnings.warn("WARNING:Circle-Map does not know if the input bam is queryname sorted\n Please check that, the output would be unexpected otherwise")
+                print("As sanity check, sort your bam file queryname with the following command:\n\n\tsamtools sort -n -o output.bam input.bam")
+
+
+
+
+        circle_sv_reads = ps.AlignmentFile(self.working_dir + self.output_bam, "wb", template=raw_bam)
+        #change the SO tag of the header to unsorted
+
+        #modify the tag to unsorted
+        if 'HD' in raw_bam.header == True:
+            circle_sv_reads.header['HD']['SO'] = 'unsorted'
+
+        if self.verbose >=3:
+            print("Extracting circular structural variants")
         #timing
         begin = time.time()
 
 
         #cache read1. this speed-ups the search
         read1 = ''
+
+        processed_reads = 0
         for read in raw_bam:
+
+            if self.verbose >=3:
+                processed_reads +=1
+
+
+                if (processed_reads/1000000).is_integer() == True:
+                    partial_timer = time.time()
+                    partial_time = (partial_timer - begin)/60
+                    print("Processed %s reads in %s mins" % (processed_reads,round(partial_time,3)))
+
             if read.is_read1:
                 read1 = read
             else:
@@ -53,55 +113,108 @@ class readExtractor:
                                 #aligned to the same chromosome
                                 if read1.reference_id == read2.reference_id:
 
-                                    circle_sv_reads.write(read1)
-                                    circle_sv_reads.write(read2)
+                                    #check that extraction is not turn off
+                                    if self.no_discordants == False:
+                                        circle_sv_reads.write(read1)
+                                        circle_sv_reads.write(read2)
+                                    else:
+                                        continue
                             else:
                                 #if the leftmost mapping condition is not met check if they are soft-clipped
                                 if is_soft_clipped(read1) == True:
 
-                                    circle_sv_reads.write(read1)
-                                    circle_sv_reads.write(read2)
+
+                                    # check that extraction is not turn off
+                                    if self.no_soft_clipped == False:
+
+                                        circle_sv_reads.write(read1)
+                                        circle_sv_reads.write(read2)
+                                    else:
+                                        continue
 
                                 else:
                                     #check hard-clipped
                                     if is_hard_clipped(read1) == True:
-                                        circle_sv_reads.write(read1)
+
+                                        # check that extraction is not turn off
+                                        if self.no_hard_clipped == False:
+
+                                            circle_sv_reads.write(read1)
+                                        else:
+                                            continue
+
 
                                 if is_soft_clipped(read2) == True:
-                                    circle_sv_reads.write(read2)
-                                    circle_sv_reads.write(read1)
+
+                                    # check that extraction is not turn off
+                                    if self.no_soft_clipped == False:
+
+                                        circle_sv_reads.write(read2)
+                                        circle_sv_reads.write(read1)
+                                    else:
+                                        continue
 
                                 else:
 
                                     #check hard-clipped
                                     if is_hard_clipped(read2) == True:
-                                        circle_sv_reads.write(read2)
+
+                                        # check that extraction is not turn off
+                                        if self.no_hard_clipped == False:
+
+                                            circle_sv_reads.write(read2)
+
+                                        else:
+
+                                            continue
 
 
                         else:
                             #check soft-clipped if R2F1 orientation is not met
                             if is_soft_clipped(read1) == True:
 
-                                circle_sv_reads.write(read1)
-                                circle_sv_reads.write(read2)
+                                # check that extraction is not turn off
+                                if self.no_soft_clipped == False:
+
+                                    circle_sv_reads.write(read1)
+                                    circle_sv_reads.write(read2)
+
+                                else:
+
+                                    continue
 
                             else:
                                 #check hard-clipped
                                 if is_hard_clipped(read1) == True:
 
-                                    circle_sv_reads.write(read1)
+
+                                    # check that extraction is not turn off
+                                    if self.no_hard_clipped == False:
+                                        circle_sv_reads.write(read1)
+
+                                    else:
+
+                                        continue
 
 
                             if is_soft_clipped(read2) == True:
 
-                                circle_sv_reads.write(read2)
-                                circle_sv_reads.write(read1)
+                                # check that extraction is not turn off
+                                if self.no_soft_clipped == False:
 
+                                    circle_sv_reads.write(read2)
+                                    circle_sv_reads.write(read1)
+
+                                else:
+
+                                    continue
 
         end = time.time()
 
-        print("finished extracting reads. Elapsed time:",(end-begin)/60,"mins")
-        print("Thanks for using Circle-Map")
+        if self.verbose >=3:
+
+            print("finished extracting reads. Elapsed time:",(end-begin)/60,"mins")
+            print("Thanks for using Circle-Map")
 
 
 
