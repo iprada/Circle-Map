d124b0622defff796d9dd10f7460fcb65447dab1
iprada <iprada@sund.ku.dk>
2017-11-24 11:35:40 +0100

    created new version of the realigner

--- tests.py
+++ tests.py
@@ -1,8 +1,27 @@
+from __future__ import division
 
-processed_reads = 0
-print(type(processed_reads / 1000000))
-if (processed_reads / 1000000).is_integer() == True:
-    print("aaa")
+from skbio.alignment import local_pairwise_align_ssw
+from skbio import DNA
+from Bio import pairwise2
+import time
+
+begin = time.time()
 
 
+for i in range(0,1000):
+    a = local_pairwise_align_ssw(DNA("ACTAAGGCTCTCTACCCCTCTCAGAGA"),DNA("ACTAAGGCTCCTAACCCCCTTTTCTCAGA"))
+end = time.time()
 
+print("Ellapsed time: using skbio",(end-begin)/60)
+
+begin = time.time()
+for j in range(0,1000):
+    b = pairwise2.align.localxx("ACTAAGGCTCTCTACCCCTCTCAGAGA", "ACTAAGGCTCCTAACCCCCTTTTCTCAGA")
+
+
+a = local_pairwise_align_ssw(DNA("ACTAAGGCTCTCTACCCCTCTCAGAGA"),DNA("ACTAAGGCTCCTAACCCCCTTTTCTCAGA"))
+print(a)
+b = pairwise2.align.localxx("ACTAAGGCTCTCTACCCCTCTCAGAGA", "ACTAAGGCTCCTAACCCCCTTTTCTCAGA")
+print(b)
+end = time.time()
+print("Ellapsed time: using biopython",(end-begin)/60)
