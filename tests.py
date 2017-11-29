314f775d3a6340d4c864937bb14078b581e68284
iprada <iprada@sund.ku.dk>
2017-11-29 09:39:23 +0100

    played around with test file for the ssw tests

--- tests.py
+++ tests.py
@@ -8,20 +8,8 @@ import time
 begin = time.time()
 
 
-for i in range(0,1000):
-    a = local_pairwise_align_ssw(DNA("ACTAAGGCTCTCTACCCCTCTCAGAGA"),DNA("ACTAAGGCTCCTAACCCCCTTTTCTCAGA"))
-end = time.time()
 
-print("Ellapsed time: using skbio",(end-begin)/60)
-
-begin = time.time()
-for j in range(0,1000):
-    b = pairwise2.align.localxx("ACTAAGGCTCTCTACCCCTCTCAGAGA", "ACTAAGGCTCCTAACCCCCTTTTCTCAGA")
+a,b,c,d = local_pairwise_align_ssw(DNA("ACTAAGGCTCTCTACCCCTCTCAGAGA"),DNA("ACTAAGGCTCCTAACCCCCTTTTCTCAGA"))
 
 
-a = local_pairwise_align_ssw(DNA("ACTAAGGCTCTCTACCCCTCTCAGAGA"),DNA("ACTAAGGCTCCTAACCCCCTTTTCTCAGA"))
-print(a)
-b = pairwise2.align.localxx("ACTAAGGCTCTCTACCCCTCTCAGAGA", "ACTAAGGCTCCTAACCCCCTTTTCTCAGA")
-print(b)
-end = time.time()
-print("Ellapsed time: using biopython",(end-begin)/60)
+
