resolved bug in coverage computation

utils.py
@@ -1201,11 +1201,11 @@ def iteration_merge(only_discordants,results,fraction,splits,score,sc_len,bam,af
         else:
             if int(interval[3]) >= splits:
                 start_cov = bam.count_coverage(contig=interval.chrom,
-                                               start=int(interval.start), stop=int(interval.start) + sc_len,
+                                               start=int(interval.start), stop=int(interval.start) + 1,
                                                quality_threshold=0, read_callback='nofilter')
 
                 end_cov = bam.count_coverage(contig=interval.chrom,
-                                             start=int(interval.end) - sc_len, stop=int(interval.end),
+                                             start=int(interval.end) - 1, stop=int(interval.end),
                                              quality_threshold=0, read_callback='nofilter')
                 start_cov_mean = np.mean(np.array([start_cov[0], start_cov[1], start_cov[2], start_cov[3]]).sum(axis=0))
                 end_cov_mean = np.mean(np.array([end_cov[0], end_cov[1], end_cov[2], end_cov[3]]).sum(axis=0))