8d7e02a2629874e1f37d3884c110dabcc979aa70
iprada <iprada@sund.ku.dk>
2020-03-13 18:02:04 +0100

    change version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #MIT License
 #
-#Copyright (c) 2019 Iñigo Prada Luengo
+#Copyright (c) 2019 Inigo Prada Luengo
 #
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
